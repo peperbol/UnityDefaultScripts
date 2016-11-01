59818b78a9ecf26102a1a326db4c280446651bbe
Pepijn Willekens <pepijnwillekens@gmail.com>
2016-11-01 15:48:23 +0100

    added delegate vector edit

--- scripts/VectorExtension.cs
+++ scripts/VectorExtension.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 //by Pepijn Willekens
 // https://github.com/peperbol
@@ -21,7 +22,7 @@ public static class VectorExtension {
 
     public static Vector3 SetZ(this Vector3 parent, float newZ)
     {
-        return new Vector3(parent.z, parent.y, newZ);
+        return new Vector3(parent.x, parent.y, newZ);
     }
 
     //Vector2
@@ -33,6 +34,17 @@ public static class VectorExtension {
 
     public static Vector2 SetY(this Vector2 parent, float newY)
     {
-        return new Vector2(parent.y, newY);
+        return new Vector2(parent.x, newY);
     }
+
+    public static Vector2 ChangeX(this Vector2 parent, FloatEdit edit)
+    {
+        return new Vector2(edit(parent.x), parent.y);
+    }
+
+    public static Vector2 ChangeY(this Vector2 parent, FloatEdit edit)
+    {
+        return new Vector2(parent.x, edit(parent.y));
+    }
+    public delegate float FloatEdit(float input);
 }
