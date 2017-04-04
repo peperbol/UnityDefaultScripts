7cbbd97a1bcda1ea859e1197ebceb8db9dc9203f
Pepijn Willekens <pepijnwillekens@gmail.com>
2017-04-04 14:49:55 +0200

    fixed vector extensions

--- scripts/VectorExtension.cs
+++ scripts/VectorExtension.cs
@@ -10,17 +10,17 @@ public static class VectorExtension {
 
     //Vector3
 
-    public static Vector3 SetX(this Vector3 parent, float newX)
+    public static Vector3 ChangeX(this Vector3 parent, float newX)
     {
         return new Vector3(newX, parent.y, parent.z);
     }
 
-    public static Vector3 SetY(this Vector3 parent, float newY)
+    public static Vector3 ChangeY(this Vector3 parent, float newY)
     {
-        return new Vector3(parent.y, newY, parent.z);
+        return new Vector3(parent.x, newY, parent.z);
     }
 
-    public static Vector3 SetZ(this Vector3 parent, float newZ)
+    public static Vector3 ChangeZ(this Vector3 parent, float newZ)
     {
         return new Vector3(parent.x, parent.y, newZ);
     }
@@ -41,12 +41,12 @@ public static class VectorExtension {
     }
     //Vector2
 
-    public static Vector2 SetX(this Vector2 parent, float newX)
+    public static Vector2 ChangeX(this Vector2 parent, float newX)
     {
         return new Vector2(newX, parent.y);
     }
 
-    public static Vector2 SetY(this Vector2 parent, float newY)
+    public static Vector2 ChangeY(this Vector2 parent, float newY)
     {
         return new Vector2(parent.x, newY);
     }
